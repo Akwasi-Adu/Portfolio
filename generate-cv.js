4f3f398781a1eedc53751fd38100b271aca2a6e2
student8@cronustrainingc.onmicrosoft.com <student8@cronustrainingc.onmicrosoft.com>
2025-04-15 19:11:06 +0000

    CV page Changes

---
+++ generate-cv.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  const page = await browser.newPage();
+
+  // Visit your live CV page
+  await page.goto('https://akwasi.dev/cv.html', { waitUntil: 'networkidle0' });
+
+  await page.pdf({
+    path: 'Akwasi_CV.pdf',
+    format: 'A4',
+    printBackground: true,
+    margin: { top: '30px', bottom: '30px', left: '20px', right: '20px' }
+  });
+
+  await browser.close();
+})();
